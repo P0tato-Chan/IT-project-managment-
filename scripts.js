eb640c6cfb17d8b9204622a9ca6a995c8b6ff7be
P0tato-Chan <140554048+P0tato-Chan@users.noreply.github.com>
2025-03-09 13:14:40 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
   async function list() {
-    var e = document.getElementById("tableSelect").value;
+    var e = document.getElementById("tableSelect").text;
   const endpoint = '/data-api/rest/' + e;
   const response = await fetch(endpoint);
   const data = await response.json();
