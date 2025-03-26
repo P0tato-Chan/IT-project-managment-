120e0357d4c8b2a3544179136051b19f71aa0803
P0tato-Chan <140554048+P0tato-Chan@users.noreply.github.com>
2025-03-26 15:58:46 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -13,7 +13,7 @@ async function get() {
 
   if (f = 'id'){
     const id = document.getElementById("search").value
-    const endpoint = `/data-api/rest/` + e + '/Id';
+    const endpoint = `/data-api/rest/` + e + '_id/Id';
     const response = await fetch(`${endpoint}/${id}`);
     const result = await response.json();
     console.table(result.value);
